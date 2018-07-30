59c6159f135ea031753b32ef4eb9774574744be7
seba <villeaho@tutanota.com>
2018-07-30 21:30:54 +0300

    README.md

---
+++ README.md
@@ -0,0 +1,1 @@
+A Ruby stock picker that takes in an array of stock prices for each day, then returns the best day to buy and sell.
---
+++ stockPicker.rb
@@ -0,0 +1,13 @@
+def stockPicker(array)
+	i = 0
+	array.each do |j|
+		puts  j
+		
+		i += 1
+	end
+
+end
+
+
+
+puts stockPicker([17,3,6,9,15,8,6,1,10])
