39c7e61a7524e0403a1e53c1199d591174b651ba
seba <villeaho@tutanota.com>
2018-08-01 00:48:07 +0300

    Added stock picker array which takes an array. It has a while loop that pushes the results of stockCounter method to profits, which is an array of buy and sell differences. It also pushes the max profit from each buy and sell to max_nums array. stockCounter method takes the arrray and a number. It subtracts each day from the number that comes after the number, and pushes it to an array and returns it.

--- stockPicker.rb
+++ stockPicker.rb
@@ -1,13 +1,47 @@
+
+
 def stockPicker(array)
 	i = 0
-	array.each do |j|
-		puts  j
-		
+	profits = []
+	p array
+	max_nums = []
+
+	while i < array.length
+		profits.push(stockCounter(array, i))
+		max_nums.push(((stockCounter(array, i)).max))
 		i += 1
 	end
 
+	puts "profits.max = #{profits.max}"
+	puts
+	p "max_nums" + max_nums.to_s
+	puts
+	p profits.map { |e| e  }
+
+	high_prof = max_nums.max
+	puts high_prof
+	high_prof_index = max_nums.index(high_prof)
+	puts "index of max_nums highest profit = #{high_prof_index}"
+	puts (profits[high_prof].index(high_prof))
+
+		
+end
+
+def stockCounter(array, num)
+	sums = []
+	j = 0
+	while j < array.length	
+		if array[j + num] == nil
+			break
+		end
+
+		sums.push(array[j + num] - array[num])
+		j += 1
+
+	end
+	sums
 end
 
 
 
-puts stockPicker([17,3,6,9,15,8,6,1,10])
+p stockPicker([17,3,6,9,15,8,6,1,10])
